1c4bb071b9862cc6f699a6c744f2cd497a071ac0
CrossScar <crossscar2@outlook.com>
2023-08-15 16:21:36 -0700

    Fix Linux, Add FPS Counter on Web

--- web/src/main.cpp
+++ web/src/main.cpp
@@ -1,11 +1,25 @@
 #include <emscripten.h>
+#include <SDL2/SDL.h>
 #include "../../src/main.hpp"
 
+unsigned int a = SDL_GetTicks();
+unsigned int b = SDL_GetTicks();
+double delta = 0;
+
 void web_update() {
-    update();
-    if (quit) {
-        emscripten_cancel_main_loop();
-        quit_sdl();
+    a = SDL_GetTicks();
+    delta = a - b;
+
+    if (delta > 1000 / MAX_FPS) {
+        fps = 1000 / delta;
+        b = a;
+
+        update();
+
+        if (quit) {
+            emscripten_cancel_main_loop();
+            quit_sdl();
+        }
     }
 }
 
